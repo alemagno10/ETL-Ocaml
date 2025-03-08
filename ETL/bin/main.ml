bedf7a5df6b7cfbcac71fd59af60211de04bd88f
alemagno10 <alemagno03@gmail.com>
2025-03-08 22:34:04 +0000

    feat: output to csv

--- ETL/bin/main.ml
+++ ETL/bin/main.ml
@@ -1,8 +1,8 @@
 open Item_reader
 open Order_reader
 open Data_processing
-(* open Save_output *)
-open Types
+open Save_output
+(* open Types *)
 
 let () =
   let filename1 = Sys.getcwd () ^ "/data/order_item.csv" in
@@ -12,22 +12,23 @@ let () =
   let orders = read_order_csv filename2 in
 
   let combined_values = inner_join items orders in
-  let filtered_combined_values = filter_by "Cancelled" 'O' combined_values in
+  let filtered_combined_values = filter_by "" ' ' combined_values in
   let processed_amount = items_amount_processing filtered_combined_values in 
   let grouped_processed_amount = group_by_order_id processed_amount in
 
-  List.iter (fun c ->
+  (* List.iter (fun c ->
     match c with 
     | Joined {order; item} ->
     Printf.printf "OrderID: %d, ProductID: %d, Quantity: %d, Price: %.2f, Tax: %.2f, ID: %d, ClientID: %d, Date: %s, Status: %s, Origin: %c\n"
     item.order_id item.product_id item.quantity item.price item.tax order.id order.client_id order.order_date order.status order.origin
-  ) grouped_processed_amount ;;
+  ) grouped_processed_amount ;; *)
   
+  let output = generate_output grouped_processed_amount in
+  let output_filename = Sys.getcwd () ^ "/data/output.csv" in
+  to_csv output output_filename ;;
 
-
-  (* let output = generate_output grouped_processed_amount in
   
-  List.iter (fun o ->
+  (* List.iter (fun o ->
     Printf.printf "OrderID: %d, Amount: %.2f, TaxAmount: %.2f\n"
       o.order_id o.amount o.tax_amount)
-    output  *)
+    output  *) 
--- ETL/bin/save_output.ml
+++ ETL/bin/save_output.ml
@@ -6,3 +6,18 @@ let generate_output (comb: combined list) : output list =
     | Joined {order; item} -> 
       {order_id = order.id ; amount = item.price ; tax_amount = item.tax}
   ) comb ;;
+
+let to_csv (out: output list) (filename: string) : unit = 
+  let file = open_out filename in
+  let csv_out = Csv.to_channel file in
+
+  let rows = ["order_id"; "amount"; "tax_amount"] :: 
+    List.map (fun o -> 
+      [string_of_int o.order_id ; string_of_float o.amount ; string_of_float o.tax_amount]
+    ) out 
+  in
+
+  Csv.output_all csv_out rows;
+
+  close_out file ;;
+  
